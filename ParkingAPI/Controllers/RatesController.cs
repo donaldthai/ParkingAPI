291405ffa12cee612c347c17c83f59d5947f73ff
Donald Thai <donald.thai@gmail.com>
2018-06-30 15:55:17 -0700

    - Set up skeleton to determine parking rates from query

--- ParkingAPI/Controllers/RatesController.cs
+++ ParkingAPI/Controllers/RatesController.cs
@@ -53,15 +53,48 @@ namespace ParkingAPI.Controllers
                 //2015-07-01T07:00:00Z to 2015-07-01T12:00:00Z
                 DateTime d1 = ParseIsoDateTime(startDateTime);
                 DateTime d2 = ParseIsoDateTime(endDateTime);
-                Console.WriteLine(d1);
-                Console.WriteLine(d2);
+                //Console.WriteLine(d1);
+                //Console.WriteLine(d2);
 
-                return "ok"; //return price
+                return GetParkingRate(d1, d2); //return price
             } catch (Exception ex) {
                 Console.WriteLine(ex);
             }
 
             return "unavailable";
+        }
+
+        /// <summary>
+        /// Gets the parking rate for the start and end datetime query.
+        /// A rate must completely encapsulate a datetime range for it to be 
+        /// available.
+        /// 
+        /// </summary>
+        /// <returns>The parking rate. Otherwise, returns "unavailable".</returns>
+        /// <param name="start">Starting datetime of query.</param>
+        /// <param name="end">Ending datetime of query.</param>
+        private string GetParkingRate(DateTime start, DateTime end) {
+            /*
+            {
+                "days": "mon,tues,thurs",
+                "times": "0900-2100",
+                "price": 1500
+            },
+            */
+
+            //1. Determine all the days from query
+            // check if the days from query match up to the days of
+            // the current rate we're looking at
+
+            //2. Determine if query time is within the time span,
+            // greater than start time,
+            // and less than end time
+
+            //3. Finally, if both 1 and 2 are ok, then return rate
+            // otherwise, return "unavailable"
+
+
+            return null;
         }
 
         /// <summary>
